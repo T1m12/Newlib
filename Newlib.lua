9b580bd91016b7cfea206083c0e348266f9eb4ed
T1m12 <99375378+T1m12@users.noreply.github.com>
2023-04-09 08:20:08 +0700

    Update and rename Newlib to Newlib.lua

--- Newlib.lua
+++ Newlib.lua
@@ -15,7 +15,7 @@ local library = {
 		easingDirection = Enum.EasingDirection.Out
 	},
 	colors = {
-		main = Color3.fromRGB(173, 63, 204),
+		main = Color3.fromRGB(0, 255, 0)
 		background = Color3.fromRGB(40, 40, 40),
 		outerBorder = Color3.fromRGB(15, 15, 15),
 		innerBorder = Color3.fromRGB(73, 63, 73),

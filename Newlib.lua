45f3f1dd4c9f531e432baaeee603352c98f9ace2
T1m12 <99375378+T1m12@users.noreply.github.com>
2023-04-09 08:23:34 +0700

    Update Newlib.lua

--- Newlib.lua
+++ Newlib.lua
@@ -15,7 +15,7 @@ local library = {
 		easingDirection = Enum.EasingDirection.Out
 	},
 	colors = {
-		main = Color3.fromRGB(0, 255, 0)
+		main = Color3.fromRGB(203, 63, 200)
 		background = Color3.fromRGB(40, 40, 40),
 		outerBorder = Color3.fromRGB(15, 15, 15),
 		innerBorder = Color3.fromRGB(73, 63, 73),
